1246158284a87d762496ee7a73fa7dcf42e59335
Zakarya Ali <zakarya.ali@majority.com>
2025-02-19 20:21:31 +0100

    add chat tab and events header logo

---
+++ fe-geoffray/app/(tabs)/chat.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, Image, Platform } from 'react-native';
+
+import { Collapsible } from '@/components/Collapsible';
+import { ExternalLink } from '@/components/ExternalLink';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { IconSymbol } from '@/components/ui/IconSymbol';
+
+export default function TabTwoScreen() {
+  return (
+    <ParallaxScrollView headerImage={undefined} headerBackgroundColor={{
+          dark: '',
+          light: ''
+      }}>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">What is the ideal gift for...</ThemedText>
+      </ThemedView>
+          <ThemedText type="link">Learn more</ThemedText>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
