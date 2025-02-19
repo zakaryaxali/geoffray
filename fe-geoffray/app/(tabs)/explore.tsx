21fe5c5622e3f2111c2f16cca56832819d85dd97
Zakarya Ali <zakarya.ali@majority.com>
2025-02-19 20:13:56 +0100

    update tabs

--- fe-geoffray/app/(tabs)/explore.tsx
+++ fe-geoffray/app/(tabs)/explore.tsx
@@ -7,7 +7,7 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 
-export default function TabTwoScreen() {
+export default function ChatScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
--- fe-geoffray/components/HelloWave.tsx
+++ fe-geoffray/components/HelloWave.tsx
@@ -26,7 +26,7 @@ export function HelloWave() {
 
   return (
     <Animated.View style={animatedStyle}>
-      <ThemedText style={styles.text}>👋</ThemedText>
+      <ThemedText style={styles.text}>🎁</ThemedText>
     </Animated.View>
   );
 }
